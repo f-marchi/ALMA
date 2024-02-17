af023248ac027b931563e39a05f49b8d8f0aef29
Francisco Marchi <fmarchi500@gmail.com>
2024-02-17 18:50:52 -0500

    add utils folder to gitignore

--- source/__init__.py
+++ source/__init__.py
@@ -1,10 +1,9 @@
 __all__ = ['cox_lasso', 'clinical_data_cleanup',
-           'data_visualization', 'pacmap_functions', 'bokeh_plots']
+           'data_visualization', 'bokeh_plots']
 
 from source import cox_lasso
 from source import clinical_data_cleanup
 from source import data_visualization
-from source import pacmap_functions
 from source import bokeh_plots
 
 
